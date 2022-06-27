387a73d02611a89d3c8effcd8b89602fb712fd64
Sam <Sam@DESKTOP-06BUCHL>
2022-06-27 19:48:59 -0400

    Add project files.

---
+++ SCM1/SCM1.cpp
@@ -0,0 +1,14 @@
+//Samuel Bryan
+//CIS 1202 800
+//June 27th, 2022
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+
+    system("pause");
+    return 0;
+}
